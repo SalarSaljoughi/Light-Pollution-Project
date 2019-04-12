c7d698119282eda583a78c24466c95e85d7f6ca2
SalarSaljoughi <saljoughi.salar@gmail.com>
2019-04-12 11:10:18 -0600

    Update logic.js

--- logic.js
+++ logic.js
@@ -9,7 +9,7 @@ mymap.doubleClickZoom.disable();
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
   maxZoom: 18,
-  id: 'mapbox.streets',
+  id: 'mapbox.high-contrast',
   accessToken: API_KEY
 }).addTo(mymap)
 
